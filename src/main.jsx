8a37850df1198b6c152e4c4a4d11c822c257031b
Axel <mandon.axel25@gmail.com>
2024-08-01 15:10:17 +0200

    Add Footer

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { Header } from './Components/Header/index.jsx'
 import { Home } from './Pages/Home/Index.jsx'
+import { Footer } from './Components/Footer/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <>
       <Header />
       <Home />
+      <Footer/>
     </>
   </React.StrictMode>,
 )
