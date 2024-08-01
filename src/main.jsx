de7b9530450b209b5111f4aeef22d539c179c716
Axel <mandon.axel25@gmail.com>
2024-08-01 14:42:51 +0200

    Home page Html

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { Header } from './Components/Header/index.jsx'
+import { Home } from './Pages/Home/Index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <>
+      <Header />
+      <Home />
+    </>
   </React.StrictMode>,
 )
